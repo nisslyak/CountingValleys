e53614ff35ef2a34578e81af115afabfe30296b1
nisslyak <nissopodolyak@gmail.com>
2024-05-31 06:19:48 +0300

    Adding the files

---
+++ Solution/Solution/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Numerics;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, string path)
+    {
+
+        int count = 0;
+        int level = 0;
+
+        foreach ( char c in path)
+        {
+            if (c == 'D')
+            {
+                level--;
+            }
+            else if (c == 'U')
+            {
+                level++;
+                if (level == 0)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
+
+
+
+
+   
+
+}
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+//        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        /*textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();*/
+    }
+}
