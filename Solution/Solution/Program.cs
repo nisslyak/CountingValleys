d89c051454a3f069db77fd44241539fd1ae0b621
nisslyak <nissopodolyak@gmail.com>
2024-06-03 10:51:21 +0300

    Added Textwriter back

--- Solution/Solution/Program.cs
+++ Solution/Solution/Program.cs
@@ -16,15 +16,6 @@ using System.Numerics;
 class Result
 {
 
-    /*
-     * Complete the 'countingValleys' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts following parameters:
-     *  1. INTEGER steps
-     *  2. STRING path
-     */
-
     public static int countingValleys(int steps, string path)
     {
 
@@ -48,23 +39,14 @@ class Result
         }
         return count;
     }
-
-
-
-
-
-
-   
-
 }
-
 
 
 class Solution
 {
     public static void Main(string[] args)
     {
-//        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
         int steps = Convert.ToInt32(Console.ReadLine().Trim());
 
@@ -72,9 +54,9 @@ class Solution
 
         int result = Result.countingValleys(steps, path);
 
-        /*textWriter.WriteLine(result);
+        textWriter.WriteLine(result);
 
         textWriter.Flush();
-        textWriter.Close();*/
+        textWriter.Close();
     }
 }
